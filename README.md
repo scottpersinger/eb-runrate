d416913ea5990d8d24687755a7b9a350163de714
Scott Persinger <scottpersinger@gmail.com>
2016-03-02 20:03:25 -0800

    Code cleanup and README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # eb-runrate
+
+A simple [EventBus](https://github.com/heroku/eventbus) consumer example in Node.js.
+Connects to the `api-events` stream on the Event Bus, and waits for `formation`
+update events which indicate dyno scaling events. The current dyno usage is stored
+in Redis with the total dyno count. A leaderboard of apps by total dynos is also
+kept in Redis in a sorted set. 
+
+The node process supports a single `/topapps` endpoint which returns the current
+leaderboard. The index html page queries the leaderboard every few seconds
+and draws the leaderboard as a word cloud.
+
+![Screenshot](/static/screenshot.png)
+
--- index.js
+++ index.js
@@ -1,58 +1,40 @@
 #!/usr/bin/env node
-var WebSocket = require('faye-websocket'),
-    fs = require('fs'),
-    redisMod = require("redis"),
-    redis = redisMod.createClient(process.env['REDIS_URL']),
-    express = require('express'),
-    app = express(),
-    http = require('http'),
-    server = http.createServer(app);
+var fs          = require('fs'),
+    http        = require('http'),
+    express     = require('express'),
+    app         = express(),
+    WebSocket   = require('faye-websocket'),
+    redisMod    = require("redis"),
+    redis       = redisMod.createClient(process.env['REDIS_URL']),
+    server      = http.createServer(app);
 
 app.use(express.static('static'));
 
-redis.on("error", function (err) {
-    console.log("Redis error " + err);
-});
-
-DEFAULT_SOCKET_URL = "wss://eventbus-sub.api.herokai.com"
 LEADERBOARD_LIMIT = 30
 
 function connect_eventbus(initial_offsets) {
-    client = new WebSocket.Client(DEFAULT_SOCKET_URL);
+    EVENTBUS_SOCKET_URL = "wss://eventbus-sub.api.herokai.com";
 
-    var stream = 'api-events';
-    var authentication = process.env['EB_AUTH']; 
-    var state = 'pending';
-    var connection_id = null;
+    var client = new WebSocket.Client(EVENTBUS_SOCKET_URL);
 
     client.on('error', function(error) {
         console.log('Connection Error: ', error);
     });
      
     client.on('open', function(event) {
-        state = 'connected';
-
         console.log('WebSocket Client Connected');
         client.on('close', function() {
-            console.log('echo-protocol Connection Closed');
+            console.log('WS Connection Closed');
         });
 
         // utils
-        function announce(data) {
-            data['id'] = connection_id;
-            console.log("Sending ", data);
-            client.send(JSON.stringify(data));
-        }
-
-        function expect_message(expected_status, callback) {
+        function expect_status(expected_status, callback) {
             client.on('message', function(event) {
                 var payload = JSON.parse(event.data);
                 if (payload['status'] == expected_status) {
-                    console.log("Received payload: ", payload);
+                    console.log("Received " + expected_status);
                     callback(payload);
-                } //else {
-                  //  console.log("Unexpected payload ", payload);
-                //}
+                }
             });
         }
 
@@ -72,52 +54,62 @@ function connect_eventbus(initial_offsets) {
 
         function process_formation(message) {
             var app = message.data.app.name;
-            //console.log(app + " lag is " + (calc_delta(message.published_at)/1000) + " seconds ");
-            var old_form = {};
+            var formation = {};
+
+            // Update combined app formation in Redis, and sum up dyno total
             redis.get(app + "/formation", function(err, payload) {
                 if (!err && payload) {
-                    old_form = JSON.parse(payload);
+                    formation = JSON.parse(payload);
                 }
-                old_form.timestamp = message.updated_at;
-                if (old_form.dynos == undefined) {
-                    old_form.dynos = {}
+                formation.timestamp = message.updated_at;
+                if (formation.dynos == undefined) {
+                    formation.dynos = {}
                 }
-                old_form.dynos[message.data.size] = message.data.quantity;
+                formation.dynos[message.data.size] = message.data.quantity;
+
+                // Total up dynos of all sizes
                 var total = 0;
-                for (var k in old_form.dynos) {
-                    total += old_form.dynos[k];
+                for (var k in formation.dynos) {
+                    total += formation.dynos[k];
                     redis.zadd(['topapps',-total,app])
                     redis.zremrangebyrank(['topapps', LEADERBOARD_LIMIT, -1])
                 }
-                //console.log(old_form);
-                redis.set(app + "/formation", JSON.stringify(old_form));
+                redis.set(app + "/formation", JSON.stringify(formation));
             });
         }
-
-        function print_leaderboard() {
-            redis.zrange(['topapps', 0, -1, "WITHSCORES"], function(err, data) {
-                console.log("Leaders: ", data);
-            });
-        }
-        setInterval(function() {console.log("heatbeat..")}, 5000);
-
-        var filter_app = 'ql-test'; //'quizlive-react';
 
         var partition_offsets = {};
 
 
-        // Protocol
-        expect_message('ready', function(payload) {
-            connection_id = payload["id"];
-            state = 'ready';
+        // EventBus protocol
+        //
+        // Once we open the websocket, then there is a message exchange to setup our
+        // subscriber. The server will send message packets with a 'state'
+        // attribute indicating the current handshake state.
+        //
+        // open ->
+        //   ready ->
+        //     [send stream, authentication, partition positions]
+        //     streaming -->
+        //       message -->
+        //         [handle event message]
+        expect_status('ready', function(payload) {
+            var connection_id  = payload["id"];
+            var stream         = 'api-events';
+            var authentication = process.env['EB_AUTH'];
 
-            announce({client:'Node sample', version:'0.1', 
+            console.log("Subscribing to " + stream);
+
+            client.send(JSON.stringify({
+                      client:'Eb-Runrate', 
+                      version:'0.1', 
+                      id: connection_id,
                       stream:stream, 
                       authentication:authentication, 
-                      state:initial_offsets ? new Buffer(initial_offsets).toString("base64") : null});
+                      state:initial_offsets ? new Buffer(initial_offsets).toString("base64") : null}
+                      ));
 
-            expect_message('streaming', function(payload) {
-                state = 'streaming';
+            expect_status('streaming', function(payload) {
 
                 console.log("STARTING STREAM LISTENER...");
                 client.on('message', function(event) {
@@ -127,20 +119,8 @@ function connect_eventbus(initial_offsets) {
 
                     var message = payload.body;
                     if (is_formation(message)) {
-                        //console.log(message);
                         process_formation(message);
                     }
-
-                    /*
-                    if (payload.body.data && payload.body.data.app && payload.body.data.app.name) {
-                        if (!filter_app || filter_app == payload.body.data.app.name) {
-                            console.log(payload);
-                            var delta = calc_delta(payload.body.published_at);
-                            console.log("[" + delta + " secs ago] - ", payload.body.data.app.name + " - " + 
-                                payload.body.action + ":" + payload.body.resource);
-                        }
-                    }*/
-
                 });
 
             });
@@ -150,9 +130,18 @@ function connect_eventbus(initial_offsets) {
 
 }
 
+// == Setup Redis
+
+redis.on("error", function (err) {
+    console.log("Redis error " + err);
+});
+
 redis.get("__partition_offsets", function(err, initial_offsets) {
+    // Connect to the EventBus
     connect_eventbus(initial_offsets);
 });
+
+// == Setup Express API to return top apps
 
 app.get('/topapps', function(request, response) {
     redis.zrange(['topapps', 0, -1, "WITHSCORES"], function(err, data) {
